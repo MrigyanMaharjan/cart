4a70daff57f3c4cd3fae55def7841c02338ca67b
Mrigyan <mrigyanmaharjan@gmail.com>
2024-03-08 08:28:57 +0545

    navbar made with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar.jsx';
+import Items from './components/Items.jsx';
+import Home from './components/Home.jsx';
+import About from './components/About.jsx';
+import More from './components/More.jsx';
+import Contact from './components/Contact.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>This is a cart app</div>
-  )
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path='/home' element={<Items />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/more' element={<More />} />
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </>
+    </Router>
+  );
 }
 
-export default App
+export default App;
