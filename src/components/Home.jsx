236bdc2b7f12b268f9a61af1ae380c4253ec50a2
Mrigyan <mrigyanmaharjan@gmail.com>
2024-03-14 00:03:32 +0545

    Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { Link } from 'react-router-dom'
-
 const Home = () => {
   return (
 
     <div className='pt-[10vh] h-screen w-screen flex items-center gap-3 flex-col justify-center'>
-
-<video className='videoTag h-screen w-screen absolute' autoPlay loop muted>
-        <source src="./components/shop.mp4" type='video/mp4' />
-        {/* Add additional <source> tags for other video formats (e.g., webm, ogg) if needed */}
-      </video>
-     <p className='text-2xl uppercase'> Welcome to my store</p>
-     <Link to={"/items"} > <button className='bg-slate-100 border-2 p-2 rounded-full hover:bg-black hover:text-white border-black duration-150  '>Enter store</button></Link>
+<div className='absolute -z-30 opacity-0.1'>
+  <video className='h-[90vh] w-screen object-cover' src="https://media.istockphoto.com/id/1136267636/video/shenzhen-city-famous-mall-interior-walking-panorama-4k-timelapse-china.mp4?s=mp4-640x640-is&k=20&c=tqkvaX7jSjkH2nFAdHBFWUmz08XMACNvMnRiAX5MOHw=" autoPlay loop muted></video>
+</div>
+<section className=' bg-transparent border-2 flex items-center justify-center flex-col border-black backdrop-blur-lg p-[4rem] rounded-xl text-white'>
+     <p className='text-2xl uppercase duration-200 p-6'> Welcome to my store</p>
+     <Link to={"/items"} > <button className='bg-black border-2 p-3 rounded-full hover:bg-white  hover:text-black border-black duration-200  '>Enter store</button></Link>
+     </section>
     </div>
+    
   )
 }
 
